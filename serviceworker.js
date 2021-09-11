8a4ee3508424720cba03d95d8e11b60e533419e2
JP Eiler <jp.eiler97@gmail.com>
2021-09-11 13:51:52 -0500

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "version-5";
 const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
-console.log(this);
+
 //Install SW
 this.addEventListener("install", (event) => {
   event.waitUntil(
